2027d0bbdf8038c5550ddfc7e10c6beb79a269df
laikeatwoon <137790785+laikeatwoon@users.noreply.github.com>
2023-06-26 22:20:26 +0800

    Update reorder-app.py

--- reorder-app.py
+++ reorder-app.py
@@ -1,15 +1,32 @@
 import streamlit as st
 import pandas as pd
 
+
 def load_data(uploaded_file):
-  data = pd.read_csv(uploaded_file)
+  data = pd.read_excel(uploaded_file)
   return data
+
+def extract_data(data):
+  new_data = pd.DataFrame(columns=['Product Code', 'Unit Sold', 'Balance Stock'])
+  i = 0
+  for index, row in data.iterrows():
+    product_code = data.at[index, "Unnamed: 1"]
+    unit_sold = data.at[index, "Unnamed: 40"]
+    balance_stock = data.at[index, "Unnamed: 61"]
+
+    if isinstance(product_code, str) and isfloat(unit_sold):
+        new_data.loc[i] = [product_code, abs(unit_sold), balance_stock]
+        i = i + 1
+
+  return new_data
+
   
 def main():
-  uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+  uploaded_file = st.file_uploader("Choose a Excel file", type="xlsx")
   if uploaded_file:
     data = load_data(uploaded_file)
-    st.write(data)
+    new_data = extract_data(data)
+    st.write(new_data)
 
 
 
