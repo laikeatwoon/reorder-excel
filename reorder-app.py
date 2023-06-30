614b00cc6140730f647ce95f24aa9511c2227e04
lai keat woon <laikeatwoon@gmail.com>
2023-06-30 10:56:58 +0800

    third commit

--- reorder-app.py
+++ reorder-app.py
@@ -5,8 +5,15 @@
 # ### Import Libraries
 
 # %%
+#!pip install -r requirements.txt
+
 import streamlit as st
 import pandas as pd
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 # %% [markdown]
 # ### Define sub functions
@@ -20,6 +27,7 @@ def isInt(value):
   except ValueError:
     return False
   
+#Load Excel File  
 def load_data(uploaded_file):
   data = pd.read_excel(uploaded_file)
   return data 
@@ -46,47 +54,95 @@ def extract_data(data):
     return new_data
 
 
+# %% [markdown]
+# ### Extract Reorder Data
+
 # %%
-def extract_data_V2(data):
-  
-  new_data = pd.DataFrame(columns=['Product Code', 'Unit Sold', 'Balance Stock'])
-  i = 0
-  for index, row in data.iterrows():
-    product_code = data.at[index, 'Unnamed: 1']
-    unit_sold = data.at[index, 'Unnamed: 40']
-    balance_stock = data.at[index, 'Unnamed: 61']
-
-    if isinstance(product_code, str) and isInt(unit_sold):
-        new_data.loc[i] = [product_code, abs(unit_sold), balance_stock]
-        i = i + 1
-
-  #Convert the data from Float to Int      
-  new_data[['Unit Sold', 'Balance Stock']] = new_data[['Unit Sold', 'Balance Stock']].astype(int)
-  
-  return new_data
-
 def extract_reorder_data(new_data):
   reorder_data = new_data.query('`Unit Sold` >= `Balance Stock`')
   return reorder_data
+
+# %% [markdown]
+# ### Extract Data from google sheet
+
+# %%
+def extract_google_sheet(sheet_name_range):
+
+  keyfile_dic = st.secrets["keyfile"]
+  creds = None
+  creds = service_account.Credentials.from_service_account_info(keyfile_dic)
+
+  service = build('sheets', 'v4', credentials=creds)
+
+  # Call the Sheets API
+  sheet = service.spreadsheets()
+  result = sheet.values().get(spreadsheetId=st.secrets["SAMPLE_SPREADSHEET_ID"],
+                              range=sheet_name_range).execute()
+  
+  data = result.get('values', [])
+  headers = data.pop(0)
+
+  df = pd.DataFrame(data, columns=headers)
+  df.dropna(how='all', inplace=True)
+
+  return df
 
 # %% [markdown]
 # ### Main Function
 
 # %%
 def main():
-  st.header("Please Order Stocks Display in the Table")
-  uploaded_file = st.file_uploader("Choose a Excel file", type="xlsx")
   
-  if uploaded_file:
-    try:
-      data = load_data(uploaded_file)
-      new_data = extract_data(data)
-      reorder_data = extract_reorder_data(new_data)
-      st.table(reorder_data)
-    except Exception as e:
+  st.set_page_config(layout="wide")
+
+  col1, col2 = st.columns([2, 3])
+  
+  with col1:
+
+    with st.expander("DF Items"):
+      try:
+        google_data = extract_google_sheet("Loose Cargo!A1:C70")
+        st.dataframe(google_data, use_container_width=True)
+      except Exception as e:
+        st.warning("Not able to load Google Sheet.")  
+    
+    with st.expander("Shandong Items"):
+      try:
+        google_data = extract_google_sheet("Shandong!A1:C70")
+        st.dataframe(google_data, use_container_width=True)
+      except Exception as e:
+        st.warning("Not able to load Google Sheet.") 
+
+    with st.expander("Taiwan Glass"):
+      try:
+        google_data = extract_google_sheet("Taiwan!A1:C70")
+        st.dataframe(google_data, use_container_width=True)
+      except Exception as e:
+        st.warning("Not able to load Google Sheet.") 
+
+    with st.expander("Lug Cap"):
+      try:
+        google_data = extract_google_sheet("Lug Cap!A1:C70")
+        st.dataframe(google_data, use_container_width=True)
+      except Exception as e:
+        st.warning("Not able to load Google Sheet.") 
+       
+  with col2:
+    st.header("Please Order Stocks Display in the Table")
+    uploaded_file = st.file_uploader("Choose a Excel file", type="xlsx")
+    
+    if uploaded_file:
+      try:
+        data = load_data(uploaded_file)
+        new_data = extract_data(data)
+        reorder_data = extract_reorder_data(new_data)
+        st.table(reorder_data)
+      except Exception as e:
         st.warning("The file is not in the correct format.")
 
 if __name__ == "__main__":
     main()
+
+
 
 
