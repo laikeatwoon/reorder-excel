fddefd3ceecd674a8a61554d2f64a60c6476f43d
lai keat woon <laikeatwoon@gmail.com>
2025-04-16 15:33:20 +0800

    Updated

--- reorder-app.py
+++ reorder-app.py
@@ -158,7 +158,7 @@ def main():
   st.set_page_config(layout="wide")
   
   # This code displays the title of the app
-  st.title("📦 Reorder App")
+  st.title(":package: Reorder App")
   
   #create a empty google_data_product_code list in a session state
   if 'google_data_product_list' not in st.session_state:
